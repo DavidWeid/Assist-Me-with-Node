e79d62efa830a21caeba02b4c77d70324368ab09
David Weid <david.weid.2@gmail.com>
2019-02-04 12:12:15 -0500

    finished basic setup

--- liri.js
+++ liri.js
@@ -4,4 +4,9 @@ var keys = require("./keys.js");
 
 var spotify = new spotify(keys.spotify);
 
+var moment = require("moment");
+moment().format();
+
+require("dotenv").config();
+
 //Commands: 'concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'
